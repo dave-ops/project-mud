c178e0e80578be564be4fcdcad1bc9b2da7e6d9b
dave-ops <david.moley@gmail.com>
2024-12-04 13:50:41 -0600

    more fixes

--- node-mud/src/Item.ts
+++ node-mud/src/Item.ts
@@ -1,6 +1,7 @@
 import Character from './Character'; // Assuming you've created the Character model
 import Room from './Room'; // Assuming you've created the Room model
 import { AffectLocation } from './enums/AffectLocation';
+import { IAffect } from './interfaces/IAffect';
 import ICharacter from './interfaces/ICharacter'; 
 import { IItem } from './interfaces/IItem'; 
 
