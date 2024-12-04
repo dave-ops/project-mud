26124a3342fee34c68be0643e2550db3513452b0
dave-ops <david.moley@gmail.com>
2024-12-04 10:08:07 -0600

    i fixed it for grok

--- node-mud/eslint.config.mjs
+++ node-mud/eslint.config.mjs
@@ -1,36 +1,13 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { FlatCompat } from '@eslint/eslintrc';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import parser from '@typescript-eslint/parser';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      project: './tsconfig.json',
-      tsconfigRootDir: __dirname,
-      ecmaVersion: 2020,
-      sourceType: 'module',
-    },
-    plugins: {
-      '@typescript-eslint': tseslint,
-    },
-  },
-});
-
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    ...compat.extends('eslint:recommended')[0],
-    ...compat.extends('plugin:@typescript-eslint/recommended')[0],
-    rules: {
-      // Add your custom rules here
-      'indent': ['error', 4],
-    },
-  },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
